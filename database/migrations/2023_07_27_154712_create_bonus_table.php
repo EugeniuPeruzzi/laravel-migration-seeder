bf6a140e2bcde69b8c33b666f474fc16e29efae2
EugeniuPeruzzi <jaka1997@live.it>
2023-07-27 17:56:22 +0200

    going through bonus, created table called 'bonus'

---
+++ database/migrations/2023_07_27_154712_create_bonus_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bonus', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda',10);
+            $table->string('start_station',50);
+            $table->string('end_station',50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->smallInteger('ticket_code');
+            $table->tinyInteger('number_vagons');
+            $table->boolean('in_time');
+            $table->boolean('canceled');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bonus');
+    }
+};
