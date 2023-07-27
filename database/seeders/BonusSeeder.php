7ae733bd6f9c24c1de0d838c1681a9fed566828f
EugeniuPeruzzi <jaka1997@live.it>
2023-07-27 18:10:05 +0200

    created new seeder and conected to faker

---
+++ database/seeders/BonusSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Faker\Provider\en_Us\Person;
+
+use App\Models\Train;
+class BonusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
