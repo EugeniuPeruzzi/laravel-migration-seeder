f39b53561fec1314a12802a0ae8671d2a1a221bc
EugeniuPeruzzi <jaka1997@live.it>
2023-07-27 16:19:49 +0200

    added seeder installed Faker

---
+++ database/seeders/TrainSeeders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Faker\Provider\en_Us\Person;
+
+use App\Models\Train;
+
+class TrainSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+    }
+}
